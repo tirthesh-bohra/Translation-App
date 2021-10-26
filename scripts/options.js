2f4c9556f5d4e688a4c62b2b2531c7f2aff8a698
tirthesh-bohra <90921054+tirthesh-bohra@users.noreply.github.com>
2021-10-26 23:52:37 +0530

    added auto language detection

--- scripts/options.js
+++ scripts/options.js
@@ -1,16 +1,17 @@
 async function getLang() {
+    let select = document.getElementById("resultLang");
+    let searchLang = document.getElementById("searchLang");
     let res = await fetch(`https://libretranslate.de/languages`);
     let data = await res.json();
-    appendLang(data);
+    appendLang(data, select);
+    appendLang(data, searchLang);
 }
-function appendLang(arr) {
-    let select = document.getElementById("resultLang")
+function appendLang(arr, container) {
     arr.forEach(({code, name}) => {
         let option = document.createElement("option");
         option.value = code;
         option.textContent = name;
-        select.append(option);
+        container.append(option);
     });
 }
-
 export {getLang, appendLang}
--- scripts/translation.js
+++ scripts/translation.js
@@ -2,12 +2,21 @@ async function translate() {
     let output = document.getElementById("output");
     output.textContent = "";
     let input = document.getElementById("input").value;
+    if(!input) {
+        return;
+    }
     let lang = document.getElementById("resultLang").value;
+    let searchLang = document.getElementById("searchLang").value;
+    if(searchLang != "auto") {
+        var source = searchLang;
+    } else {
+        var source = await detectLang(input);
+    }
     let res = await fetch(`https://libretranslate.de/translate`, {
         method: 'POST',
         body: JSON.stringify({
             q: input,
-            source: "en",
+            source,
             target: lang,
         }),
         headers: {
@@ -18,4 +27,19 @@ async function translate() {
     output.textContent = data.translatedText;
 }
 
-export {translate}
+
+async function detectLang(input) {
+    let res = await fetch(`https://libretranslate.de/detect`, {
+        method: "POST",
+        body: JSON.stringify({
+            q: input,
+        }),
+        headers: { 
+            "Content-Type": "application/json" 
+        }
+    });
+    let data = await res.json();
+    return data[0].language;
+}
+
+export {translate, detectLang}
